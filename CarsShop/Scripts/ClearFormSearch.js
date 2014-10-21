12956dbf9443b80a56e029d81095908dffc26c55
Sergey <alykardz40@gmail.com>
2014-10-21 22:10:39 +0300

    refactoring ClearFormSearch

--- CarsShop/Scripts/ClearFormSearch.js
+++ CarsShop/Scripts/ClearFormSearch.js
@@ -1,18 +1,19 @@
 ﻿function ClearSearchForm() {
-    $('#Mark').val("");
-    $('#Series').val("");
-    $('#BodyType').val("");
-    $('#FuelType').val("");
-    $('#Transmission').val("");
-    $('#Price_min').val("");
-    $('#Price_max').val("");
-    $('#Year_min').val("");
-    $('#Year_max').val("");
-    $('#country').val("");
-    $('#town').val("");
-    $('#Drive').val("");
-    $('#Color').val("");
-    $('#Distance_min').val("");
-    $('#Distance_max').val("");
+    //$('#Mark').val("");
+    //$('#Series').val("");
+    //$('#BodyType').val("");
+    //$('#FuelType').val("");
+    //$('#Transmission').val("");
+    //$('#Price_min').val("");
+    //$('#Price_max').val("");
+    //$('#Year_min').val("");
+    //$('#Year_max').val("");
+    //$('#country').val("");
+    //$('#town').val("");
+    //$('#Drive').val("");
+    //$('#Color').val("");
+    //$('#Distance_min').val("");
+    //$('#Distance_max').val("");
+    $('select').val("");
     $("input:checkbox").removeAttr("checked");    
 }
