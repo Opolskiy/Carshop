3c2b2b572e0954fb57f190c1c93d1b897d589072
Sergey <alykardz40@gmail.com>
2014-11-16 22:39:52 +0300

    Comment functional complete

--- CarsShop/Controllers/UserActionsController.cs
+++ CarsShop/Controllers/UserActionsController.cs
@@ -14,13 +14,64 @@ namespace CarsShop.Controllers
     {
         private static Mutex mutex = new Mutex();
 
-        public ActionResult Comments(Guid CarId)
+        public ActionResult DeleteComment(Guid CommentId)
+        {
+            ApplicationDbContext Db = new ApplicationDbContext();
+            
+            var com = Db.Comments.FirstOrDefault(c => c.CommentId == CommentId);
+            var car = Db.Cars.FirstOrDefault(m => m.CarId == com.CarId);
+            if ( User.Identity.Name == com.Author )
+            {
+                Db.Comments.Remove(com);
+                Db.SaveChanges();
+            }
+            else { return Content("error"); }
+            return RedirectToAction("Comments", "UserActions", car);
+        }
+
+        public ActionResult NewComment(Comment comment)
+        {
+            comment.Author = User.Identity.Name;
+            comment.Date = DateTime.Now;
+            comment.CommentId = Guid.NewGuid();
+            ApplicationDbContext db = new ApplicationDbContext();
+            db.Comments.Add(comment);
+            db.SaveChanges();
+            Car c = db.Cars.FirstOrDefault(s => s.CarId == comment.CarId);
+            return RedirectToAction("Comments", "UserActions", c);
+        }
+
+        public ActionResult _CommentPartial(Guid CarId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List <Comment> commentList = new List<Comment>();
+            commentList = db.Comments.Where(c => c.CarId == CarId).ToList();
+            commentList.Sort(delegate(Comment x, Comment y) 
+            { 
+                if(x.Date < y.Date) return -1;
+                if ( x.Date > y.Date ) return 1;
+                else return 0;
+            });
+            ViewBag.ListComments = commentList;
+            Comment com = new Comment();
+            com.CarId = CarId;
+            return PartialView(com);
+        }
+
+        public ActionResult Viewsplusplus(Guid CarId)
         {
             var prop = typeof(Car).GetProperty("CountViews");
             ApplicationDbContext Db = new ApplicationDbContext();
             var car = Db.Cars.FirstOrDefault(c => c.CarId == CarId);
             prop.SetValue(car, car.CountViews + 1);
             Db.SaveChanges();
+            return RedirectToAction("Comments", new { CarId = CarId });
+        }
+
+        public ActionResult Comments(Guid CarId)
+        {
+            ApplicationDbContext Db = new ApplicationDbContext();
+            var car = Db.Cars.FirstOrDefault(c => c.CarId == CarId);
             return View(car);
         }
 
--- CarsShop/Models/Comment.cs
+++ CarsShop/Models/Comment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarsShop.Models
 {
@@ -11,6 +12,7 @@ namespace CarsShop.Models
         public Guid CommentId { get; set; }
         public string Author { get; set; }
         public DateTime Date { get; set; }
+        [Display(Name = "Ваш отзыв")]
         public string Data { get; set; }
     }
 }
